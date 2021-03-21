a8cdfaf48354281c016a78e27c7cb5bf34f54f1a
dovid746 <71200689+dovid746@users.noreply.github.com>
2021-03-21 16:12:18 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,66 @@
+
+//changes # display upon clicking
+function one() {
+
+	document.getElementById('numDisp').innerHTML = 1;
+}
+
+function two() {
+
+	document.getElementById('numDisp').innerHTML = 2;
+}
+
+function three() {
+
+	document.getElementById('numDisp').innerHTML = 3;
+}
+
+function four() {
+
+	document.getElementById('numDisp').innerHTML = 4;
+}
+
+function five() {
+
+	document.getElementById('numDisp').innerHTML = 5;
+}
+
+function six() {
+
+	document.getElementById('numDisp').innerHTML = 6;
+}
+
+function seven() {
+
+	document.getElementById('numDisp').innerHTML = 7;
+}
+
+function eight() {
+
+	document.getElementById('numDisp').innerHTML = 8;
+}
+
+function nine() {
+
+	document.getElementById('numDisp').innerHTML = 9;
+}
+
+function zero() {
+
+	document.getElementById('numDisp').innerHTML = 0;
+}
+
+//only 2 numbers
+let numOne = 0;
+let numTwo = 0;
+
+//operators default state 
+var plus = false;
+let minus = false;
+let multiply = false;
+let divide = false;
+
+function calcButtonPlus() {
+
+	var plus = true;
+}
